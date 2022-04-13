cbc2f016fee2f35885a32f1acab3449caab07944
mark <mddoyne@asu.edu>
2022-04-13 13:00:53 -0600

    Save

--- index.js
+++ index.js
@@ -59,3 +59,23 @@ function say(greeting, firstName)
   console.log("greeting: ", greeting);
   console.log(`${greeting}, ${firstName}!`);
 }
+
+function add(x, y)
+{
+  return x + y;
+}
+console.log(add(1, 2));
+
+function add(x, y)
+{
+  console.log(x+y);
+}
+add(1,2);
+
+function say(greeting, firstName)
+{
+  console.log("I was called!");
+  return `${greeting}, ${firstName}!`;
+}
+
+console.log(say("Howdy", "partner"));
